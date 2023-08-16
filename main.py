fbb77adcbe00c6b807dc832ef60dd2b99e68b291
unknown <sayeh.mirghasemi@gmail.com>
2023-08-16 22:26:20 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_y = 0
+while True:
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    frame_height , frame_width , _ = frame.shape
+
+    rgb_frame  = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand)
+            landmarks = hand.landmark
+            for id, landmark in enumerate(landmarks):
+                x = int(landmark.x*frame_width)
+                y = int(landmark.y*frame_height)
+
+                if id ==8:
+                    index_x = screen_width / frame_width * x
+                    index_y = screen_height / frame_height * y
+                    pyautogui.moveTo(index_x,index_y)
+                if id ==4:
+                    thumb_x = screen_width / frame_width*x
+                    thumb_y = screen_height / frame_height*y
+                    if abs(index_y - thumb_y) <20:
+                        pyautogui.click()
+                    pyautogui.sleep(1)
+
+                if id ==12:
+                    middle_x = screen_width / frame_width * x
+                    middle_y = screen_height / frame_height * y
+                    if abs(middle_y - thumb_y) < 10:
+                        pyautogui.leftClick()
+                    pyautogui.sleep(1)
+
+
+    print(hands)
+    cv2.imshow('Virtual Mouse', frame)
+    cv2.waitKey(1)
